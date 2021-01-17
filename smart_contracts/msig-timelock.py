83dcd88c01da7f01bab87486d8b887dfc08c3724
Keefer Taylor <keefer.t@gmail.com>
2021-01-17 11:54:53 +0900

    rotate keys (#7)
    Co-authored-by: Keefer Taylor <keefer@keefertaylor.com>

--- smart_contracts/msig-timelock.py
+++ smart_contracts/msig-timelock.py
@@ -116,6 +116,45 @@ class MultiSigTimelock(sp.Contract):
       # Add to timelock.
       self.data.timelock[self.data.nonce] = (sp.now, lambdaToExecute)
 
+    # Rotate keys, assuming the request has been properly signed.
+    # Param:
+    # - signedKeyRotationRequest (SIGNED_KEY_ROTATION_REQUEST_TYPE) The request to submit.
+    @sp.entry_point
+    def rotateKeys(self, signedKeyRotationRequest):
+      # Destructure input params
+      sp.set_type(signedKeyRotationRequest, SIGNED_KEY_ROTATION_REQUEST_TYPE)
+      signatures, keyRotationRequest = sp.match_pair(signedKeyRotationRequest)
+
+      # Destructure execution request
+      chainId, innerPair = sp.match_pair(keyRotationRequest)
+      nonce, keyData = sp.match_pair(innerPair)
+
+      # Verify ChainID
+      sp.verify_equal(chainId, sp.chain_id, "BAD_CHAIN_ID")
+      
+      # Verify Nonce
+      sp.verify(nonce == self.data.nonce + 1, "BAD_NONCE")
+
+      # Count valid signatures
+      validSignaturesCounter = sp.local('valid_signatures_counter', sp.nat(0))
+      sp.for operator_public_key in self.data.operator_public_keys:
+        # Check if the given public key is in the signatures list.
+        keyHash = sp.hash_key(operator_public_key)
+        sp.if signatures.contains(keyHash):
+          sp.verify(sp.check_signature(operator_public_key, signatures[keyHash], sp.pack(keyRotationRequest)), "BAD_SIGNATURE")
+          validSignaturesCounter.value += 1
+        
+      # Verify that enough signatures were provided.
+      sp.verify(validSignaturesCounter.value >= self.data.signers_threshold, "TOO_FEW_SIGS")
+
+      # Increment nonce.
+      self.data.nonce += 1
+
+      # Update key data
+      threshold, keyList = sp.match_pair(keyData)
+      self.data.signers_threshold  = threshold
+      self.data.operator_public_keys = keyList
+
     # Execute a request in the timelock.
     # Pamrams:
     # - nonce (nat) The identifier of the nonce to execute.
@@ -169,6 +208,8 @@ class StoreValueContract(sp.Contract):
 ################################################################
 # addExecutionRequest
 ################################################################
+
+# TODO(keefertaylor): These tests must test that the nonce has been updated.
 
 @sp.add_test(name = "addExecutionRequest - succeeds with all signatures")
 def test():
@@ -470,63 +511,438 @@ def test():
     valid = False
   )
 
-  @sp.add_test(name = "addExecutionRequest - does not count invalid signatures")
-  def test():
-    scenario = sp.test_scenario()
+@sp.add_test(name = "addExecutionRequest - does not count invalid signatures")
+def test():
+  scenario = sp.test_scenario()
 
-    # GIVEN a set of an accounts
-    alice = sp.test_account("alice")
-    bob = sp.test_account("bob")
-    charlie = sp.test_account("charlie")
-    dan = sp.test_account("dan")
-    eve = sp.test_account("eve")
+  # GIVEN a set of an accounts
+  alice = sp.test_account("alice")
+  bob = sp.test_account("bob")
+  charlie = sp.test_account("charlie")
+  dan = sp.test_account("dan")
+  eve = sp.test_account("eve")
 
-    # AND a timelock multisig contract with a threshold of 3/3
-    threshhold = 3
-    multiSigContract = MultiSigTimelock(
-      signers_threshold = threshhold,
-      operator_public_keys = [ alice.public_key, bob.public_key, charlie.public_key]
-    )
-    scenario += multiSigContract
+  # AND a timelock multisig contract with a threshold of 3/3
+  threshhold = 3
+  multiSigContract = MultiSigTimelock(
+    signers_threshold = threshhold,
+    operator_public_keys = [ alice.public_key, bob.public_key, charlie.public_key]
+  )
+  scenario += multiSigContract
 
-    # AND a chain id.
-    chainId = sp.chain_id_cst("0x9caecab9")
+  # AND a chain id.
+  chainId = sp.chain_id_cst("0x9caecab9")
 
-    # AND a store value contract with the multisig as the admin.
-    storeContract = StoreValueContract(value = 0, admin = multiSigContract.address)
-    scenario += storeContract
+  # AND a store value contract with the multisig as the admin.
+  storeContract = StoreValueContract(value = 0, admin = multiSigContract.address)
+  scenario += storeContract
 
-    # AND a lambda is to update the value
-    newValue = 1
-    def updateLambda(unitParam):
-      sp.set_type(unitParam, sp.TUnit)
-      storeContractHandle = sp.contract(sp.TNat, storeContract.address, 'replace').open_some()
-      sp.result([sp.transfer_operation(newValue, sp.mutez(0), storeContractHandle)])
+  # AND a lambda is to update the value
+  newValue = 1
+  def updateLambda(unitParam):
+    sp.set_type(unitParam, sp.TUnit)
+    storeContractHandle = sp.contract(sp.TNat, storeContract.address, 'replace').open_some()
+    sp.result([sp.transfer_operation(newValue, sp.mutez(0), storeContractHandle)])
 
-    # AND a payload is signed by 3 signatures but 2 are invalid.
-    nonce = 1
-    executionRequest = (chainId, (nonce, updateLambda))
-    executionRequestBytes = sp.pack(executionRequest)
+  # AND a payload is signed by 3 signatures but 2 are invalid.
+  nonce = 1
+  executionRequest = (chainId, (nonce, updateLambda))
+  executionRequestBytes = sp.pack(executionRequest)
 
-    charlieSignature = sp.make_signature(charlie.secret_key, executionRequestBytes)
-    danSignature = sp.make_signature(dan.secret_key, executionRequestBytes)
-    eveSignature = sp.make_signature(eve.secret_key, executionRequestBytes)
+  charlieSignature = sp.make_signature(charlie.secret_key, executionRequestBytes)
+  danSignature = sp.make_signature(dan.secret_key, executionRequestBytes)
+  eveSignature = sp.make_signature(eve.secret_key, executionRequestBytes)
 
-    #  WHEN the request is sent to the multisignature contract.
-    signatures = {
-      charlie.public_key_hash: charlieSignature,
-      dan.public_key_hash: danSignature,
-      eve.public_key_hash: eveSignature,
-    }
-    signedExecutionRequest = (signatures, executionRequest)
-    now = sp.timestamp(123)
+  #  WHEN the request is sent to the multisignature contract.
+  signatures = {
+    charlie.public_key_hash: charlieSignature,
+    dan.public_key_hash: danSignature,
+    eve.public_key_hash: eveSignature,
+  }
+  signedExecutionRequest = (signatures, executionRequest)
+  now = sp.timestamp(123)
 
-    # THEN it fails.
-    scenario += multiSigContract.addExecutionRequest(signedExecutionRequest).run(
-      chain_id = chainId,
-      now = now,
-      valid = False
-    )
+  # THEN it fails.
+  scenario += multiSigContract.addExecutionRequest(signedExecutionRequest).run(
+    chain_id = chainId,
+    now = now,
+    valid = False
+  )
+
+################################################################
+# rotateKeys
+################################################################
+
+# TODO(keefertaylor): No need for value store contract.
+
+@sp.add_test(name = "rotateKeys - succeeds with all signatures")
+def test():
+  scenario = sp.test_scenario()
+
+  # GIVEN a set of an accounts
+  alice = sp.test_account("alice")
+  bob = sp.test_account("bob")
+  charlie = sp.test_account("charlie")
+  dan = sp.test_account("dan")
+  eve = sp.test_account("eve")
+
+  # AND a timelock multisig contract with a threshold of 3/5
+  threshhold = 3
+  multiSigContract = MultiSigTimelock(
+    signers_threshold = threshhold,
+    operator_public_keys = [ alice.public_key, bob.public_key, charlie.public_key, dan.public_key, eve.public_key ]
+  )
+  scenario += multiSigContract
+
+  # AND a chain id.
+  chainId = sp.chain_id_cst("0x9caecab9")
+
+  # AND a store value contract with the multisig as the admin.
+  storeContract = StoreValueContract(value = 0, admin = multiSigContract.address)
+  scenario += storeContract
+
+  # AND key data
+  newThreshold = sp.nat(2)
+
+  fred = sp.test_account("fred")
+  george = sp.test_account("george")
+  newKeyList = [ fred.public_key, george.public_key ]
+
+  newKeyData = (newThreshold, newKeyList)
+
+  # AND a payload is correctly signed by all parties.
+  nonce = 1
+  rotationRequest = (chainId, (nonce, newKeyData))
+  rotationRequestBytes = sp.pack(rotationRequest)
+
+  aliceSignature = sp.make_signature(alice.secret_key, rotationRequestBytes)
+  bobSignature = sp.make_signature(bob.secret_key, rotationRequestBytes)
+  charlieSignature = sp.make_signature(charlie.secret_key, rotationRequestBytes)
+  danSignature = sp.make_signature(dan.secret_key, rotationRequestBytes)
+  eveSignature = sp.make_signature(eve.secret_key, rotationRequestBytes)
+
+  #  WHEN the request is sent to the multisignature contract.
+  signatures = {
+   alice.public_key_hash:   aliceSignature,
+   bob.public_key_hash:     bobSignature, 
+   charlie.public_key_hash: charlieSignature,
+   dan.public_key_hash:     danSignature,
+   eve.public_key_hash:     eveSignature 
+  }
+  signedRotationRequest = (signatures, rotationRequest)
+  now = sp.timestamp(123)
+  scenario += multiSigContract.rotateKeys(signedRotationRequest).run(
+    chain_id = chainId,
+    now = now
+  )
+
+  # THEN the nonce has been updated
+  scenario.verify(multiSigContract.data.nonce == nonce)
+
+  # AND the key data has been updated.
+  scenario.verify(multiSigContract.data.signers_threshold == newThreshold)
+  scenario.verify_equal(multiSigContract.data.operator_public_keys, newKeyList)
+
+@sp.add_test(name = "rotateKeys - succeeds with threshold signatures")
+def test():
+  scenario = sp.test_scenario()
+
+  # GIVEN a set of an accounts
+  alice = sp.test_account("alice")
+  bob = sp.test_account("bob")
+  charlie = sp.test_account("charlie")
+  dan = sp.test_account("dan")
+  eve = sp.test_account("eve")
+
+  # AND a timelock multisig contract with a threshold of 3/5
+  threshhold = 3
+  multiSigContract = MultiSigTimelock(
+    signers_threshold = threshhold,
+    operator_public_keys = [ alice.public_key, bob.public_key, charlie.public_key, dan.public_key, eve.public_key ]
+  )
+  scenario += multiSigContract
+
+  # AND a chain id.
+  chainId = sp.chain_id_cst("0x9caecab9")
+
+  # AND a store value contract with the multisig as the admin.
+  storeContract = StoreValueContract(value = 0, admin = multiSigContract.address)
+  scenario += storeContract
+
+  # AND key data
+  newThreshold = sp.nat(2)
+
+  fred = sp.test_account("fred")
+  george = sp.test_account("george")
+  newKeyList = [ fred.public_key, george.public_key ]
+
+  newKeyData = (newThreshold, newKeyList)
+
+  # AND a payload is correctly signed by 3 parties.
+  nonce = 1
+  rotationRequest = (chainId, (nonce, newKeyData))
+  rotationRequestBytes = sp.pack(rotationRequest)
+
+  bobSignature = sp.make_signature(bob.secret_key, rotationRequestBytes)
+  charlieSignature = sp.make_signature(charlie.secret_key, rotationRequestBytes)
+  danSignature = sp.make_signature(dan.secret_key, rotationRequestBytes)
+
+  #  WHEN the request is sent to the multisignature contract.
+  signatures = {
+   bob.public_key_hash:     bobSignature, 
+   charlie.public_key_hash: charlieSignature,
+   dan.public_key_hash:     danSignature,
+  }
+  signedRotationRequest = (signatures, rotationRequest)
+  now = sp.timestamp(123)
+  scenario += multiSigContract.rotateKeys(signedRotationRequest).run(
+    chain_id = chainId,
+    now = now
+  )
+
+  # THEN the nonce has been updated
+  scenario.verify(multiSigContract.data.nonce == nonce)
+
+  # AND the key data has been updated.
+  scenario.verify(multiSigContract.data.signers_threshold == newThreshold)
+  scenario.verify_equal(multiSigContract.data.operator_public_keys, newKeyList)
+
+@sp.add_test(name = "rotateKeys - fails with bad nonce")
+def test():
+  scenario = sp.test_scenario()
+
+  # GIVEN a set of an accounts
+  alice = sp.test_account("alice")
+  bob = sp.test_account("bob")
+  charlie = sp.test_account("charlie")
+  dan = sp.test_account("dan")
+  eve = sp.test_account("eve")
+
+  # AND a timelock multisig contract with a threshold of 3/5
+  threshhold = 3
+  multiSigContract = MultiSigTimelock(
+    signers_threshold = threshhold,
+    operator_public_keys = [ alice.public_key, bob.public_key, charlie.public_key, dan.public_key, eve.public_key ]
+  )
+  scenario += multiSigContract
+
+  # AND a chain id.
+  chainId = sp.chain_id_cst("0x9caecab9")
+
+  # AND a store value contract with the multisig as the admin.
+  storeContract = StoreValueContract(value = 0, admin = multiSigContract.address)
+  scenario += storeContract
+
+  # AND key data
+  newThreshold = sp.nat(2)
+
+  fred = sp.test_account("fred")
+  george = sp.test_account("george")
+  newKeyList = [ fred.public_key, george.public_key ]
+
+  newKeyData = (newThreshold, newKeyList)
+
+  # AND a payload is correctly with a bad nonce
+  nonce = 4 # Obviously wrong.
+  rotationRequest = (chainId, (nonce, newKeyData))
+  rotationRequestBytes = sp.pack(rotationRequest)
+
+  bobSignature = sp.make_signature(bob.secret_key, rotationRequestBytes)
+  charlieSignature = sp.make_signature(charlie.secret_key, rotationRequestBytes)
+  danSignature = sp.make_signature(dan.secret_key, rotationRequestBytes)
+
+  # WHEN the request is sent to the multisignature contract
+  signatures = {
+   bob.public_key_hash:     bobSignature, 
+   charlie.public_key_hash: charlieSignature,
+   dan.public_key_hash:     danSignature,
+  }
+  signedRotationRequest = (signatures, rotationRequest)
+  now = sp.timestamp(123)
+
+  # THEN the call fails.
+  scenario += multiSigContract.rotateKeys(signedRotationRequest).run(
+    chain_id = chainId,
+    now = now,
+    valid = False
+  )
+
+@sp.add_test(name = "rotateKeys - fails with bad chain id")
+def test():
+  scenario = sp.test_scenario()
+
+  # GIVEN a set of an accounts
+  alice = sp.test_account("alice")
+  bob = sp.test_account("bob")
+  charlie = sp.test_account("charlie")
+  dan = sp.test_account("dan")
+  eve = sp.test_account("eve")
+
+  # AND a timelock multisig contract with a threshold of 3/5
+  threshhold = 3
+  multiSigContract = MultiSigTimelock(
+    signers_threshold = threshhold,
+    operator_public_keys = [ alice.public_key, bob.public_key, charlie.public_key, dan.public_key, eve.public_key ]
+  )
+  scenario += multiSigContract
+
+  # AND a chain id.
+  chainId = sp.chain_id_cst("0x9caecab9")
+
+  # AND a store value contract with the multisig as the admin.
+  storeContract = StoreValueContract(value = 0, admin = multiSigContract.address)
+  scenario += storeContract
+
+  # AND key data
+  newThreshold = sp.nat(2)
+
+  fred = sp.test_account("fred")
+  george = sp.test_account("george")
+  newKeyList = [ fred.public_key, george.public_key ]
+
+  newKeyData = (newThreshold, newKeyList)
+
+  # AND a payload is correctly signed by 3 parties.
+  nonce = 1
+  rotationRequest = (chainId, (nonce, newKeyData))
+  rotationRequestBytes = sp.pack(rotationRequest)
+
+  bobSignature = sp.make_signature(bob.secret_key, rotationRequestBytes)
+  charlieSignature = sp.make_signature(charlie.secret_key, rotationRequestBytes)
+  danSignature = sp.make_signature(dan.secret_key, rotationRequestBytes)
+
+  # WHEN the request is sent to the multisignature contract with an incorrect chain id
+  # THEN the request fails.
+  signatures = {
+   bob.public_key_hash:     bobSignature, 
+   charlie.public_key_hash: charlieSignature,
+   dan.public_key_hash:     danSignature,
+  }
+  signedRotationRequest = (signatures, rotationRequest)
+  now = sp.timestamp(123)
+  scenario += multiSigContract.rotateKeys(signedRotationRequest).run(
+    chain_id = sp.chain_id_cst("0x0011223344"),
+    now = now,
+    valid = False
+  )
+
+@sp.add_test(name = "rotationRequest - fails with less than threshold signatures")
+def test():
+  scenario = sp.test_scenario()
+
+  # GIVEN a set of an accounts
+  alice = sp.test_account("alice")
+  bob = sp.test_account("bob")
+  charlie = sp.test_account("charlie")
+  dan = sp.test_account("dan")
+  eve = sp.test_account("eve")
+
+  # AND a timelock multisig contract with a threshold of 3/5
+  threshhold = 3
+  multiSigContract = MultiSigTimelock(
+    signers_threshold = threshhold,
+    operator_public_keys = [ alice.public_key, bob.public_key, charlie.public_key, dan.public_key, eve.public_key ]
+  )
+  scenario += multiSigContract
+
+  # AND a chain id.
+  chainId = sp.chain_id_cst("0x9caecab9")
+
+  # AND a store value contract with the multisig as the admin.
+  storeContract = StoreValueContract(value = 0, admin = multiSigContract.address)
+  scenario += storeContract
+
+  # AND key data
+  newThreshold = sp.nat(2)
+
+  fred = sp.test_account("fred")
+  george = sp.test_account("george")
+  newKeyList = [ fred.public_key, george.public_key ]
+
+  newKeyData = (newThreshold, newKeyList)
+
+  # AND a payload is correctly signed by less than the threshold.
+  nonce = 1
+  rotationRequest = (chainId, (nonce, newKeyData))
+  rotationRequestBytes = sp.pack(rotationRequest)
+
+  bobSignature = sp.make_signature(bob.secret_key, rotationRequestBytes)
+  charlieSignature = sp.make_signature(charlie.secret_key, rotationRequestBytes)
+
+  #  WHEN the request is sent to the multisignature contract.
+  signatures = {
+  bob.public_key_hash:     bobSignature, 
+  charlie.public_key_hash: charlieSignature,
+  }
+  signedRotationRequest = (signatures, rotationRequest)
+  now = sp.timestamp(123)
+
+  # THEN it fails.
+  scenario += multiSigContract.rotateKeys(signedRotationRequest).run(
+    chain_id = chainId,
+    now = now,
+    valid = False
+  )
+
+@sp.add_test(name = "rotateKeys - does not count invalid signatures")
+def test():
+  scenario = sp.test_scenario()
+
+  # GIVEN a set of an accounts
+  alice = sp.test_account("alice")
+  bob = sp.test_account("bob")
+  charlie = sp.test_account("charlie")
+  dan = sp.test_account("dan")
+  eve = sp.test_account("eve")
+
+  # AND a timelock multisig contract with a threshold of 3/3
+  threshhold = 3
+  multiSigContract = MultiSigTimelock(
+    signers_threshold = threshhold,
+    operator_public_keys = [ alice.public_key, bob.public_key, charlie.public_key]
+  )
+  scenario += multiSigContract
+
+  # AND a chain id.
+  chainId = sp.chain_id_cst("0x9caecab9")
+
+  # AND a store value contract with the multisig as the admin.
+  storeContract = StoreValueContract(value = 0, admin = multiSigContract.address)
+  scenario += storeContract
+
+  # AND key data
+  newThreshold = sp.nat(2)
+
+  fred = sp.test_account("fred")
+  george = sp.test_account("george")
+  newKeyList = [ fred.public_key, george.public_key ]
+
+  newKeyData = (newThreshold, newKeyList)
+
+  # AND a payload is signed by 3 signatures but 2 are invalid.
+  nonce = 1
+  rotationRequest = (chainId, (nonce, newKeyData))
+  rotationRequestBytes = sp.pack(rotationRequest)
+
+  charlieSignature = sp.make_signature(charlie.secret_key, rotationRequestBytes)
+  danSignature = sp.make_signature(dan.secret_key, rotationRequestBytes)
+  eveSignature = sp.make_signature(eve.secret_key, rotationRequestBytes)
+
+  #  WHEN the request is sent to the multisignature contract.
+  signatures = {
+    charlie.public_key_hash: charlieSignature,
+    dan.public_key_hash: danSignature,
+    eve.public_key_hash: eveSignature,
+  }
+  signedRotationRequest = (signatures, rotationRequest)
+  now = sp.timestamp(123)
+
+  # THEN it fails.
+  scenario += multiSigContract.rotateKeys(signedRotationRequest).run(
+    chain_id = chainId,
+    now = now,
+    valid = False
+  )
 
 ################################################################
 # execute
